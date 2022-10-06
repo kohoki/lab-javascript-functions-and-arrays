a62a8f160574dd5fa4acc04d5c78bffb3b553519
Ralf Bonnick <ralfbonnick@gmx.net>
2022-10-06 11:09:37 +0200

    finish iteration2

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -16,7 +16,31 @@ function maxOfTwoNumbers(number1, number2)
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(array) 
+{
+  if (array.length === 0)
+  {
+    return null;
+  }
+  else if (array.length === 1)
+  {
+    return array[0];
+  }
+  else (array.length > 1)
+  {
+    let cache = "";
+    for (let index = 0; index < array.length; index +=1)
+    {
+      if (array[index].length > cache.length)
+        {
+          cache = array[index];
+        }
+    }
+    return cache;
+  }
+
+  //for (let index = 0; index < array.length; index += 1)
+}
 
 
 
